55fe54d13e9a900f2feacc3bcd9708eb53ae12cc
Archana <archanaandhale1008@gmail.com>
2023-04-20 11:44:33 +0530

    updated

---
+++ TestProject10/src/com/test/ColumSum.java
@@ -0,0 +1,29 @@
+package com.test;
+
+public class ColumSum {
+	public static void sumOfcolumn(int a[][]) {
+		int sum;
+		for(int i=0;i<a.length;i++)
+		{
+			sum=0;
+			for(int j=0;j<a[i].length;j++) {
+			sum=sum+a[j][i];
+			System.out.print(a[i][j]+" ");
+			
+		}
+		System.out.print("sum="+sum);
+		System.out.println();
+		
+	}
+}
+		
+	
+
+	public static void main(String[] args) {
+		int a[][]= {{1,2,4},{2,3,5},{3,5,6}};
+		ColumSum .sumOfcolumn(a);
+		
+
+	}
+
+}
